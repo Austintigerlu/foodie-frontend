6c4984189f2074eb604541ee166b8eea5e822c4b
austintigerlu <austintigerlu@gmail.com>
2022-06-06 11:01:53 -0400

    searchbar

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,10 +1,10 @@
-
+import SearchBar from "../SearchBar"
 import { Link } from "react-router-dom";
 
 function Nav(){
     return (
     <>
-        <nav className="bg-gray-800">
+        <nav className="bg-slate-400">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
@@ -16,21 +16,7 @@ function Nav(){
                             />
                         </div>
                         <div className="ml-10 flex items-baseline space-x-4">
-                            <Link to="/">
-                                <p className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">
-                                    Home
-                                </p>
-                            </Link>
-                            <Link to="/IM">
-                                <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                    Messaging
-                                </p>
-                            </Link>
-                            <Link to="/translate">
-                                <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                    Translate
-                                </p>
-                            </Link>
+                            <SearchBar/>
                         </div>
                         </div>
                         <div className="mr-2 flex items-end space-x-4">
