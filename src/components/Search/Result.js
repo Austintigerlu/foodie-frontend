ef81a470b1f670c142fed72be5e7fee5a3d60481
austintigerlu <austintigerlu@gmail.com>
2022-06-09 01:57:36 -0400

    register done

--- src/components/Search/Result.js
+++ src/components/Search/Result.js
@@ -9,7 +9,7 @@ function Result(props){
                 <h2>Place Name</h2>
                     <Rating/>
                 <p>$
-                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-slate-600 bg-slate-200 uppercase last:mr-0 ml-1 mr-1">
+                    <span className="text-xs font-semibold inline-block py-1 px-2 rounded text-slate-600 bg-slate-200 uppercase last:mr-0 ml-1 mr-1">
                     slate</span>
                 </p>
             </div>
